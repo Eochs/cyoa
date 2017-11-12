a78c5b930788c6c823681455ae7afc6f2f0985d2
Spencer Ochs <slochastic@gmail.com>
2017-11-12 09:45:04 -0800

    styled html and created ServeHttp method

--- cmd/cyoaweb/main.go
+++ cmd/cyoaweb/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/Eochs/cyoa"
+)
 
 func main() {
-	fmt.Println("hi")
+	port := flag.Int("port", 3001, "the port to start CYOA on")
+	filename := flag.String("file", "gopher.json", "JSON file with the CYOA story")
+	flag.Parse()
+	fmt.Printf("Using hte story in %s.\n", *filename)
+
+	f, err := os.Open(*filename)
+	if err != nil {
+		panic(err)
+	}
+
+	story, err := cyoa.JsonStory(f)
+	if err != nil {
+		panic(err)
+	}
+
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server at: %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
